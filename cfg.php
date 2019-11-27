d904bbb50b61392bac24934558972524208e5684
Essatofa12 <58127600+Essatofa12@users.noreply.github.com>
2019-11-27 21:42:14 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?
+/*isi refres token kalian*/
+$refres = "AEu4IL3XtYb6D0e_gQMBh8cAzAwBS1I4KT2OXNCc0IUiIcwMnbxy5Nk_8YiOYlQy3mtOMHYFypGszdrJzOryAZ3tMIpIy8vmg9MF18gvVX3uGOXcyFtI_a3T1klOrvhfzbdzQx4RKzI57OUXdFWmyBtK72fTz7XHVhrsM9Ss9hkgZXQrZijOvO6czpqe4Fy-2KFpC2PUSGsyEufuxYnw95J0TglsK3dxQFVN5MZ8Az4p-suJ14fRBOpq7Z0681hVQRShGg7C_XSt66wTxFO6EFsVqJU0sGdzRy3wPzU8F2l80u0jHfwKJHj7_HPT1sWU_yl0zrAqD3_W3p9aMwSxAG2WPXegpyMtdVimCV4J8K7JXB9Kt5HJVeqdUEy7XPdCPHDibkuPrzbzLbnplXA9rPO3jkoMnibW8iolIuJoFrVLXy4KRO57BnnR2VFT1vbgw4BwZYs-BAyT";
+
+/*isi email wd coinbase kalian*/
+$email_coinbase = "";
+
+/*link pas https://semawur.com/Zwiq95       */
+$pass_scrip = "good123";
+
